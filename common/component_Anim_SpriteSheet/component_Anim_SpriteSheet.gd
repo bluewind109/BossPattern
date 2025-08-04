dc89775150899c11fbfb3d75504f9fcc03d1895b
Khanh Hoang Quoc <khanh.hoangquoc91@gmail.com>
2025-08-04 17:47:12 +0700

    Fix anim glitch when switching from idle/run to attack for enemy Golem

--- common/component_Anim_SpriteSheet/component_Anim_SpriteSheet.gd
+++ common/component_Anim_SpriteSheet/component_Anim_SpriteSheet.gd
@@ -17,6 +17,7 @@ func play_anim(_anim_name: String, is_loop: bool = true):
 	if (not anim_dict.has(_anim_name)): return
 	if (current_anim == _anim_name): return
 	# print("_play_anim: ", anim_name)
+	anim_player.stop()
 	var anim_data: Variant = anim_dict[_anim_name]
 	texture = anim_data.texture
 	# hframes = anim_data.hframes
