a0417b1a4deaa479d92a129f94d68cb22219b70d
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2025-12-30 15:01:57 +0700

    * Update game camera and add smoothing

---
+++ game_camera/game_camera.gd
@@ -0,0 +1,20 @@
+extends Camera2D
+class_name GameCamera
+
+var target_position = Vector2.ZERO
+
+
+func _ready() -> void:
+	make_current()
+
+	
+func _process(delta: float) -> void:
+	_acquire_target()
+	global_position = global_position.lerp(target_position, 1.0 - exp(-delta * 10))
+
+
+func _acquire_target():
+	var player_nodes = get_tree().get_nodes_in_group("Player")
+	if player_nodes.size() > 0:
+		var player = player_nodes[0] as Node2D
+		target_position = player.global_position
