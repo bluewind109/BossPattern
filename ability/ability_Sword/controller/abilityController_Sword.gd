83c4015dc1c7987fad508bb1646b05b5a2293ab6
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2025-12-30 18:12:29 +0700

    * Update ability sword to spawn on closest enemy

--- ability/ability_Sword/controller/abilityController_Sword.gd
+++ ability/ability_Sword/controller/abilityController_Sword.gd
@@ -1,7 +1,9 @@
 extends Node
 class_name AbilityController_Sword
 
+@export var max_range: float = 50.0
 @export var sword_prefab: PackedScene
+
 @onready var timer: Timer = $timer
 
 
@@ -10,9 +12,24 @@ func _ready() -> void:
 
 
 func _on_ability_timer_finished():
-	var player_node = get_tree().get_first_node_in_group("Player") as Node2D
-	if (player_node == null): return
+	var player = get_tree().get_first_node_in_group("Player") as Node2D
+	if (player == null): return
+
+	var enemies = get_tree().get_nodes_in_group("Enemy")
+
+	# only enemies inside max range
+	enemies = enemies.filter(func(enemy: Node2D): 
+		return enemy.global_position.distance_squared_to(player.global_position) < pow(max_range, 2.0)
+	)
+	if (enemies.size() == 0): return
+
+	# closest enemy inside max range
+	enemies.sort_custom(func(a: Node2D, b: Node2D): 
+		var distance_a = a.global_position.distance_squared_to(player.global_position)
+		var distance_b = b.global_position.distance_squared_to(player.global_position)
+		return distance_a < distance_b
+	)
 
 	var sword_instance = sword_prefab.instantiate() as Ability_Sword
-	player_node.get_parent().add_child(sword_instance)
-	sword_instance.global_position = player_node.global_position
+	player.get_parent().add_child(sword_instance)
+	sword_instance.global_position = enemies[0].global_position 
