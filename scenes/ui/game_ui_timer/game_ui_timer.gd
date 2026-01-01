f5b2a9073803241f6486db0ee8d4fe9bbd901d91
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2026-01-01 18:10:53 +0700

    + Add experience bar and game timer

---
+++ scenes/ui/game_ui_timer/game_ui_timer.gd
@@ -0,0 +1,23 @@
+extends CanvasLayer
+class_name GameUiTimer
+
+@onready var game_timer: Timer = $game_timer
+@onready var label_game_time: Label = $%label_game_time
+var current_time: float = 0.0
+
+
+func _ready() -> void:
+	current_time = 0.0
+	update_game_time()
+
+
+func _process(delta: float) -> void:
+	if (!get_tree().paused):
+		current_time += delta
+	update_game_time()
+
+
+func update_game_time():
+	var total_seconds = int(current_time) % 60
+	var total_minutes = int(current_time) / 60
+	label_game_time.text = "%02d:%02d" % [total_minutes, total_seconds]
