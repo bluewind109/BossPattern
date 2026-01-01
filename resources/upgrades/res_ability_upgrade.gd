e010b88241fd708376c92b75932a51da97ea360d
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2026-01-01 19:16:02 +0700

    + Add upgrade manager and ability upgrade resource

---
+++ resources/upgrades/res_ability_upgrade.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name Res_AbilityUpgrade
+
+@export var id: String
+@export var name: String
+@export_multiline var desc: String
---
+++ scenes/manager/upgrade_manager/upgrade_manager.gd
@@ -0,0 +1,8 @@
+extends Node
+class_name UpgradeManager
+
+@export var upgrade_pool: Array[Res_AbilityUpgrade]
+
+
+func _ready() -> void:
+	pass # Replace with function body.
