e6d910ba6d605a00b422afb3cd3b13b036c669f5
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2026-01-02 12:18:19 +0700

    + Add victory screen

--- scenes/ui/game_ui_timer/game_ui_timer.gd
+++ scenes/ui/game_ui_timer/game_ui_timer.gd
@@ -1,12 +1,16 @@
 extends CanvasLayer
 class_name GameUiTimer
 
+@export var victory_screen_scene: PackedScene
+
 @onready var game_timer: Timer = $game_timer
+@onready var count_down_timer: Timer = $count_down_timer
 @onready var label_game_time: Label = $%label_game_time
 var current_time: float = 0.0
 
 
 func _ready() -> void:
+	count_down_timer.timeout.connect(_on_count_down_finished)
 	current_time = 0.0
 	update_game_time()
 
@@ -21,3 +25,8 @@ func update_game_time():
 	var total_seconds = int(current_time) % 60
 	var total_minutes = int(current_time) / 60
 	label_game_time.text = "%02d:%02d" % [total_minutes, total_seconds]
+
+
+func _on_count_down_finished():
+	var victory_screen = victory_screen_scene.instantiate()
+	add_child(victory_screen)
---
+++ scenes/ui/victory_screen/victory_screen.gd
@@ -0,0 +1,20 @@
+extends CanvasLayer
+class_name VictoryScreen
+
+@onready var restart_button: Button = $%button_restart
+@onready var quit_button: Button = $%button_quit
+
+
+func _ready() -> void:
+	get_tree().paused = true
+	restart_button.pressed.connect(_on_restart_button_pressed)
+	quit_button.pressed.connect(_on_quit_button_pressed)
+
+
+func _on_restart_button_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/game/game.tscn")
+	
+
+func _on_quit_button_pressed():
+	get_tree().quit()
