5bdf11b1ea4bf7b3e8da08bfd511213469773615
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2026-01-03 16:41:55 +0700

    * Update velocity component to work for enemy only

--- scenes/common/velocity/velocity.gd
+++ scenes/common/velocity/velocity.gd
@@ -5,37 +5,39 @@ const DECELERATION_TARGET: Vector2 = Vector2.ZERO
 
 var owner_node: CharacterBody2D
 
-@export var acceleration_coefficient: float = 1.0
-@export var deceleration_coefficient: float = 1.0
+@export var max_speed: float = 40
+@export var acceleration: float = 5
 
-@export var max_speed: float = 150.0
+var velocity: Vector2 = Vector2.ZERO
 
-var direction: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
 	pass
 
-func _physics_process(_delta: float) -> void:
-	if (owner_node == null): return
 
-	if (direction):
-		_accelerate()
-	else:
-		if (owner_node.velocity):
-			_decelerate()
-	owner_node.move_and_slide()
+func accelerate_to_player():
+	var owner_node2d = owner as Node2D
+	if (owner_node2d == null): return
+	var player = get_tree().get_first_node_in_group("Player") as Player
+	if (player == null): return
+	var direction = (player.global_position - owner_node2d.global_position).normalized()
+	accelerate_in_direction(direction)
+
+
+func accelerate_in_direction(direction: Vector2):
+	var desired_velocity = direction * max_speed
+	velocity = velocity.lerp(desired_velocity, 1 - exp(-acceleration * get_process_delta_time()))
+
+
+func move(character_body: CharacterBody2D):
+	character_body.velocity = velocity
+	character_body.move_and_slide()
+	velocity = character_body.velocity
+
 
 func set_max_speed(val: float):
 	max_speed = val
 
+
 func set_direction(val: Vector2):
-	direction = val
-
-func _accelerate():
-	var acceleration_rate: float = max_speed * acceleration_coefficient
-	var speed: Vector2 = direction.normalized() * max_speed
-	owner_node.velocity = owner_node.velocity.move_toward(speed, acceleration_rate)
-
-func _decelerate():
-	var deceleration_rate: float = max_speed * deceleration_coefficient
-	owner_node.velocity = owner_node.velocity.move_toward(DECELERATION_TARGET, deceleration_rate)
+	pass
