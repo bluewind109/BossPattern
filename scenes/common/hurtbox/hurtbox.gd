a8429e023b356d5e50dad9109173a7c0143d03ef
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2026-01-04 18:10:12 +0700

    + Add floating text for damage dealt

--- scenes/common/hurtbox/hurtbox.gd
+++ scenes/common/hurtbox/hurtbox.gd
@@ -2,6 +2,7 @@ extends Area2D
 class_name ComponentHurtbox
 
 @export var component_health: ComponentHealth
+@export var floating_text_scene: PackedScene
 
 signal damaged(amount: float)
 
@@ -13,3 +14,9 @@ func take_damage(amount: float) -> void:
 	if (component_health == null): return
 	component_health.take_damage(amount)
 	damaged.emit(amount)
+	
+	if (floating_text_scene == null): return
+	var floating_text = floating_text_scene.instantiate() as FloatingText
+	get_tree().get_first_node_in_group("foreground_layer").add_child(floating_text)
+	floating_text.global_position = global_position + Vector2.UP * 16
+	floating_text.start(str(amount))
