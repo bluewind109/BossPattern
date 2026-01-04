49d81fbe8c65080a48a484bb8f9ca96e0f4045c3
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2026-01-04 22:26:37 +0700

    + Add hit flash component

---
+++ scenes/common/hit_flash/hit_flash.gd
@@ -0,0 +1,22 @@
+extends Node
+class_name HitFlash
+
+@export var hit_flash_material: ShaderMaterial
+@export var comp_health: ComponentHealth
+@export var sprite: Sprite2D
+
+var hit_flash_tween: Tween
+
+func _ready():
+	comp_health.health_changed.connect(_on_health_changed)
+	sprite.material = hit_flash_material
+
+
+func _on_health_changed(health: float):
+	if (sprite == null): return
+	if (hit_flash_tween != null and hit_flash_tween.is_valid()): 
+		hit_flash_tween.kill()
+		
+	(sprite.material as ShaderMaterial).set_shader_parameter("lerp_percent", 1.0)
+	hit_flash_tween = create_tween()
+	hit_flash_tween.tween_property(sprite.material, "shader_parameter/lerp_percent", 0.0, 0.5)
