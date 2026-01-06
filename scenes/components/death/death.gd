9e4593088453ceaf6285bb8b6d29fe3e96c7d621
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2026-01-06 22:41:41 +0700

    + Add hit SFX to enemy on hit and on death

--- scenes/components/death/death.gd
+++ scenes/components/death/death.gd
@@ -1,23 +1,36 @@
 extends Node2D
 class_name ComponentDeath
 
+@onready var hit_audio_player: RandomAudioPlayer2D = $hit_random_audio_player_2d
+@onready var timer: Timer = $timer
+
 @export var comp_health: ComponentHealth
 @export var sprite: Sprite2D
 
 
 func _ready() -> void:
+	self.name = "death"
 	#gpu_particle.texture = sprite.texture
 	comp_health.died.connect(_on_died)
+	timer.timeout.connect(_on_timeout)
 
 
 # Remove entity from the game but still play death animation
 func _on_died():
 	if (owner == null || not owner is Node2D): return
+	timer.start()
+	
 	var spawn_pos: Vector2 = owner.global_position
 	var entities = get_tree().get_first_node_in_group("entities_layer")
 	get_parent().remove_child(self)
 	entities.add_child(self)
 	global_position = spawn_pos
+	
+	hit_audio_player.play_random()
 	# TODO play death animation
+	
 	#animation_player.play("default")
 	
+
+func _on_timeout():
+	queue_free()
--- scenes/game_object/enemy/enemy_Base.gd
+++ scenes/game_object/enemy/enemy_Base.gd
@@ -13,13 +13,16 @@ var speed_dict: Dictionary[int, float] = {}
 @onready var component_velocity: ComponentVelocity = $velocity
 @onready var component_look: ComponentLook = $look
 @onready var attack_manager: AttackManager = $attack_manager
+@onready var hit_audio_player: RandomAudioPlayer2D = $hit_random_audio_player_2d
 
 @export var mass: float = 20
 
 var is_spawning: bool = false
 var is_dead: bool = false
 
+
 func _ready() -> void:
+	component_hurtbox.damaged.connect(_on_damaged)
 	player_ref = get_tree().get_first_node_in_group("Player")
 	if (component_health): 
 		component_health.died.connect(_on_die)
@@ -29,17 +32,22 @@ func _ready() -> void:
 func init_states():
 	pass
 
+
 func init_speed_dict():
 	pass
+
 
 func init_anim_dict(_lib_name: String):
 	pass
 
+
 func bind_signals():
 	pass
 
+
 func add_states():
 	pass
+
 
 func _disable_collision():
 	if (component_hitbox):
@@ -49,9 +57,11 @@ func _disable_collision():
 		component_hurtbox.monitoring = false
 		component_hurtbox.monitorable = false
 
+
 func init_component_look(_target: Node2D):
 	if (not component_look): return
 	component_look.owner_node = _target
+
 
 func look_at_player():
 	if (not component_look): return
@@ -60,19 +70,27 @@ func look_at_player():
 	if (target_pos == null): return
 	component_look.look(target_pos)
 
+
 func set_state(state_name: String):
 	if (is_dead): return
 	state_machine.change_state(state_name)
+
 
 func get_player_position():
 	var player_node = get_tree().get_first_node_in_group("Player") as Node2D
 	if (player_node == null): return null
 	return player_node.global_position
 
+
 func get_direction_to_player() -> Vector2:
 	var player_node = get_tree().get_first_node_in_group("Player") as Node2D
 	if (player_node == null): return Vector2.ZERO
 	return (player_node.global_position - global_position).normalized()
+
+
+func _on_damaged(amount: float):
+	hit_audio_player.play_random()
+
 
 func _on_die():
 	is_dead = true
