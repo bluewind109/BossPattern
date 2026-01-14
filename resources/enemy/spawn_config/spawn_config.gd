13a2a10d758f8cc5c2d32eaea4700cdbec9c2137
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2026-01-15 00:49:37 +0700

    * Update function in spawn config

--- resources/enemy/spawn_config/spawn_config.gd
+++ resources/enemy/spawn_config/spawn_config.gd
@@ -4,5 +4,9 @@ class_name SpawnConfig
 @export var guideline: Array[Res_SpawnData]
 
 
-func get_spawn_item():
-	pass
+func get_items_by_difficulty(difficulty: int) -> Array[Res_SpawnData]:
+	var filtered_arr: Array[Res_SpawnData] = guideline.duplicate()
+	filtered_arr.filter(func(data: Res_SpawnData):
+		return 	data.difficulty_level == difficulty
+	)
+	return filtered_arr
