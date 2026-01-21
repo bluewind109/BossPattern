01d13d8dd7b54e71d316ad94666f6874accea553
Khanh Hoang Quoc <khanh.hoangquoc91@gmail.com>
2026-01-21 18:05:32 +0700

    * Update look component (WIP)

--- scenes/components/look/look.gd
+++ scenes/components/look/look.gd
@@ -1,30 +1,34 @@
 extends Node2D
 class_name ComponentLook
 
+@export var is_disabled: bool = false
+@export var use_rotation: bool = false
 @export var is_base_sprite_looking_left: bool = true
 @export var visual_node: Node2D
+
+var target_pos: Vector2
 
 func _ready() -> void:
 	pass
 
-func look(target_pos: Vector2):
-	if (is_base_sprite_looking_left):
-		if (get_owner_position().x < target_pos.x):
-			flip_direction(true)
-		else:
-			flip_direction(false)
-	else:
-		if (get_owner_position().x > target_pos.x):
-			flip_direction(true)
-		else:
-			flip_direction(false)
+func look(_target_pos: Vector2):
+	if (is_disabled): return
+	target_pos = _target_pos
+	var direction = (target_pos - get_owner_position()).normalized()
+	flip_direction(direction.x > 0) if is_base_sprite_looking_left else flip_direction(direction.x < 0)
+
 
 func flip_direction(is_flipped: bool):
 	if (!visual_node): return
-	if (is_flipped):
-		visual_node.scale.x = -1
-	else:
-		visual_node.scale.x = 1
+
+	if (use_rotation):
+		var _angle =  visual_node.get_angle_to(target_pos)
+		visual_node.rotation_degrees = fposmod(_angle, 360.0)
+		if (rad_to_deg(_angle) > 180):
+			visual_node.scale.y = -1
+
+	visual_node.scale.y = 1
+	visual_node.scale.x = -1 if is_flipped else 1
 
 
 func get_owner_position() -> Vector2:
