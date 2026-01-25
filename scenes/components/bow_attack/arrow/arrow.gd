f39913bb02b4f340acb1d844536743df923fe171
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2026-01-26 00:37:35 +0700

    + Add bow attack's script and animation

--- scenes/components/bow_attack/arrow/arrow.gd
+++ scenes/components/bow_attack/arrow/arrow.gd
@@ -4,9 +4,9 @@ class_name Arrow
 @onready var hitbox: ComponentHitbox = $%hitbox
 @onready var pivot: Marker2D = $pivot
 @onready var on_screen_notifier: VisibleOnScreenNotifier2D = $visible_on_screen_notifier_2d
+@onready var sprite: Sprite2D = $pivot/sprite_2d
 
-@export var speed: float = 75
-
+var speed: float = 150.0
 var weapon_damage: float = 1.0
 var direction: Vector2 = Vector2.ZERO
 
@@ -17,14 +17,20 @@ func _ready() -> void:
 	on_screen_notifier.screen_exited.connect(_on_screen_exited)
 
 
-func init(_direction: Vector2, _damage: float):
+func init(_direction: Vector2, _damage: float, _speed: float):
 	direction = _direction
+	speed = _speed
+	# look_at(direction)
 	hitbox.set_damage(_damage)
 
 
 func _physics_process(delta: float) -> void:
 	if (direction):
 		global_position += direction * speed * delta
+
+
+func look(_target_pos: Vector2):
+	look_at(_target_pos)
 
 
 func _on_hit(hurtbox: ComponentHurtbox, _damage: float):
---
+++ scenes/components/bow_attack/bow_attack.gd
@@ -0,0 +1,76 @@
+extends Node2D
+class_name BowAttack
+
+@export var arrow_scene: PackedScene
+@export var start_pos: Vector2 = Vector2.ZERO
+@export var arrow_spawn: Node2D
+
+@onready var pivot: Marker2D = $pivot
+@onready var animation_player: AnimationPlayer = $animation_player
+@onready var weapon_sprite: Sprite2D = $%weapon_sprite
+
+@export var attack_time: float = 0.2
+@export var return_time: float = 0.5
+@export var weapon_damage: float = 1.0
+@export var arrow_speed: float = 300.0
+
+var can_attack: bool = true
+var current_look_dir: String = "left"
+
+const ATTACK_ANIM = "attack"
+const RETURN_ANIM = "return"
+
+
+func _ready() -> void:
+	position = start_pos
+	animation_player.animation_finished.connect(_on_animation_finished)
+
+
+func _physics_process(delta: float) -> void:
+	var mouse_pos: Vector2 = get_global_mouse_position()
+	pivot.look_at(mouse_pos)
+
+	if (current_look_dir == "left" and get_global_mouse_position().x > global_position.x):
+		scale.x = 1
+		current_look_dir = "right"
+	elif (current_look_dir == "right" and get_global_mouse_position().x < global_position.x):
+		scale.x = -1
+		current_look_dir = "left"
+
+	if (Input.is_action_just_pressed("attack") and can_attack):
+		animation_player.speed_scale =\
+		animation_player.get_animation(ATTACK_ANIM).length /  attack_time
+		animation_player.play(ATTACK_ANIM)
+		can_attack = false
+
+
+func spawn_arrow() -> void:
+	if (arrow_scene == null): return
+	var arrow = arrow_scene.instantiate() as Arrow
+	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
+	if (entities_layer == null): return
+	entities_layer.add_child(arrow)
+
+	if (arrow_spawn == null): 
+		arrow.global_position = global_position
+	else:
+		arrow.global_position = arrow_spawn
+	
+	if (get_global_mouse_position().x > global_position.x):
+		arrow.pivot.scale = Vector2(1, 1)
+		arrow.rotation = pivot.rotation
+	else:
+		arrow.pivot.scale = Vector2(-1, -1)
+		arrow.rotation = pivot.rotation * -1
+
+	var _direction = (get_global_mouse_position() - global_position).normalized()
+	arrow.init(_direction, weapon_damage, arrow_speed)
+
+
+func _on_animation_finished(_anim_name: StringName):
+	if (_anim_name == ATTACK_ANIM):
+		animation_player.speed_scale =\
+		animation_player.get_animation(RETURN_ANIM).length /  return_time
+		animation_player.play(RETURN_ANIM)
+	else:
+		can_attack = true
