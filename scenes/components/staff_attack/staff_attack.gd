0a8dea3182c73f14f1250523022a23954c4448dc
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2026-01-26 00:50:09 +0700

    + Add staff attack (WIP)

---
+++ scenes/components/staff_attack/staff_attack.gd
@@ -0,0 +1,56 @@
+extends Node2D
+class_name StaffAttack
+
+@export var start_pos: Vector2 = Vector2.ZERO
+@export var projectile_spawn: Node2D
+
+@onready var pivot: Marker2D = $pivot
+@onready var animation_player: AnimationPlayer = $animation_player
+@onready var weapon_sprite: Sprite2D = $%weapon_sprite
+
+@export var attack_time: float = 0.2
+@export var return_time: float = 0.5
+@export var weapon_damage: float = 1.0
+@export var projectile_speed: float = 300.0
+
+var can_attack: bool = true
+var current_look_dir: String = "left"
+
+const ATTACK_ANIM = "attack"
+const RETURN_ANIM = "return"
+
+
+func _ready() -> void:
+	position = start_pos
+	animation_player.animation_finished.connect(_on_animation_finished)
+
+
+func _physics_process(delta: float) -> void:
+	var mouse_pos: Vector2 = get_global_mouse_position()
+	pivot.look_at(mouse_pos)
+
+	if (current_look_dir == "left" and get_global_mouse_position().x > global_position.x):
+		scale.x = 1
+		current_look_dir = "right"
+	elif (current_look_dir == "right" and get_global_mouse_position().x < global_position.x):
+		scale.x = -1
+		current_look_dir = "left"
+
+	if (Input.is_action_just_pressed("attack") and can_attack):
+		animation_player.speed_scale =\
+		animation_player.get_animation(ATTACK_ANIM).length /  attack_time
+		animation_player.play(ATTACK_ANIM)
+		can_attack = false
+
+
+func spawn_projectile() -> void:
+	pass
+
+
+func _on_animation_finished(_anim_name: StringName):
+	if (_anim_name == ATTACK_ANIM):
+		animation_player.speed_scale =\
+		animation_player.get_animation(RETURN_ANIM).length /  return_time
+		animation_player.play(RETURN_ANIM)
+	else:
+		can_attack = true
