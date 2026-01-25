d627a6b90a7fb4ff680b0d00b257724cee76468b
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2026-01-25 23:03:11 +0700

    + Add spear attack and animation

---
+++ scenes/components/spear_attack/spear_attack.gd
@@ -0,0 +1,49 @@
+extends Node2D
+class_name SpearAttack
+
+@onready var pivot: Marker2D = $pivot
+@onready var hitbox: ComponentHitbox = $%hitbox
+@onready var animation_player: AnimationPlayer = $animation_player
+@onready var spear_sprite: Sprite2D = $%spear_sprite
+
+@export var attack_time: float = 0.2
+@export var return_time: float = 0.5
+@export var weapon_damage: float = 1.0
+
+var can_attack: bool = true
+var current_look_dir: String = "left"
+
+const ATTACK_ANIM = "attack"
+const RETURN_ANIM = "return"
+
+
+func _ready() -> void:
+	animation_player.animation_finished.connect(_on_animation_finished)
+	hitbox.set_damage(weapon_damage)
+
+
+func _physics_process(delta: float) -> void:
+	var mouse_pos: Vector2 = get_global_mouse_position()
+	pivot.look_at(mouse_pos)
+
+	if (current_look_dir == "left" and get_global_mouse_position().x > global_position.x):
+		scale.x = 1
+		current_look_dir = "right"
+	elif (current_look_dir == "right" and get_global_mouse_position().x < global_position.x):
+		scale.x = -1
+		current_look_dir = "left"
+
+	if (Input.is_action_just_pressed("attack") and can_attack):
+		animation_player.speed_scale =\
+		animation_player.get_animation(ATTACK_ANIM).length /  attack_time
+		animation_player.play(ATTACK_ANIM)
+		can_attack = false
+
+
+func _on_animation_finished(_anim_name: StringName):
+	if (_anim_name == ATTACK_ANIM):
+		animation_player.speed_scale =\
+		animation_player.get_animation(RETURN_ANIM).length /  return_time
+		animation_player.play(RETURN_ANIM)
+	else:
+		can_attack = true
