b107e7580568489b30acbb2a0e885407cd677d11
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2026-01-26 16:29:54 +0700

    * Update enemy data's default ID

--- resources/enemy/enemy_data/res_EnemyData.gd
+++ resources/enemy/enemy_data/res_EnemyData.gd
@@ -2,7 +2,7 @@ extends Resource
 class_name Res_EnemyData
 
 @export var enemy_scene: PackedScene
-@export var id: EnemyDefine.ENEMY_ID = EnemyDefine.ENEMY_ID.BASE
+@export var id: EnemyDefine.ENEMY_ID
 @export var name: String
 @export var health: float = 5.0
 @export var speed: float = 25.0
