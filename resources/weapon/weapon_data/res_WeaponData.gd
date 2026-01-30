e770c03ca1d21e73793136de66f25d417c9c6116
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2026-01-30 22:58:55 +0700

    + Add max level variable for weapon data

--- resources/weapon/weapon_data/res_WeaponData.gd
+++ resources/weapon/weapon_data/res_WeaponData.gd
@@ -5,11 +5,19 @@ class_name Res_WeaponData
 @export var id: WeaponDefine.WEAPON_ID
 @export var name: String
 @export var base_damage: float = 1.0
+@export var max_level: int = 1
 @export var icons: Array[Texture2D] = []
 
 
 func do():
 	pass
+
+
+func search_icon(search_string: String) -> Texture2D:
+	for icon in icons:
+		if (icon.resource_path.contains(search_string)):
+			return icon
+	return null
 
 
 func search_icons(search_string: String) -> Array[Texture2D]:
