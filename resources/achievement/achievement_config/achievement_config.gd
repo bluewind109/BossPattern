29a3057bc4728aa8ddf1b06bfe5f5441c3baceac
Khanh Hoang Quoc <khanh.hoangquoc91@gmail.com>
2026-02-05 14:42:36 +0700

    + Add tool button to generate achievements

--- resources/achievement/achievement_config/achievement_config.gd
+++ resources/achievement/achievement_config/achievement_config.gd
@@ -1,7 +1,13 @@
+@tool
 extends Resource
 class_name AchievementConfig
 
 @export var achievements: Array[AchievementData] = []
+
+@export_tool_button("Generate achievements")
+var generate_achievements_button: Callable = generate_achievements
+
+const path: String = "res://resources/achievement/achievement_data/"
 
 
 func get_achievement_by_id(_id: AchievementDefine.ACHIEVEMENT_ID) -> AchievementData:
@@ -9,3 +15,22 @@ func get_achievement_by_id(_id: AchievementDefine.ACHIEVEMENT_ID) -> Achievement
 		if (achievement.id == _id):
 			return achievement
 	return null
+
+
+func generate_achievements():
+	print("generate_achievements")
+	achievements = []
+	var dir: DirAccess = DirAccess.open(path)
+
+	if dir:
+		dir.list_dir_begin()
+		var file_name: String = dir.get_next()
+		while file_name != "":
+			if not dir.current_is_dir() and file_name.contains(".tres"):
+				var full_path: String = path.path_join(file_name)
+				var loaded_resource: Resource = ResourceLoader.load(full_path)
+				if (loaded_resource):
+					achievements.append(loaded_resource)
+			file_name = dir.get_next()
+	else:
+		print("Error generate achievements")
